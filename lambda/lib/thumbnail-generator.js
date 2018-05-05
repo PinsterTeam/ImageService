5ec543f93391f812b078b04db8aafc3067ea89b4
scytherswings <scytherswings@gmail.com>
2018-05-05 02:01:10 -0400

    trying promises

--- lambda/lib/thumbnail-generator.js
+++ lambda/lib/thumbnail-generator.js
@@ -22,39 +22,42 @@ module.exports = class ThumbnailGenerator {
         this.imageTransformer = _.isUndefined(imageTransformer) ? ImageTransformer : imageTransformer;
     }
 
-    static parseRequestedImage(requestedImageKey, callback) {
-        const match = requestedImageKey.match(IMAGE_KEY_PATTERN_REGEX);
+    static parseRequestedImage(requestedImageKey) {
+        return new Promise((resolve) => {
+            const match = requestedImageKey.match(IMAGE_KEY_PATTERN_REGEX);
 
-        if (match === null) {
-            callback(new BadRequest(`Key: '${requestedImageKey}' is not a supported image file!`));
-        }
-        else {
-            const dimensions = match[4]; //400xauto, for example.
-            const width = match[5] === 'auto' ? null : Math.min(parseInt(match[5], 10), MAX_SIZE);
-            const height = match[6] === 'auto' ? null : Math.min(parseInt(match[6], 10), MAX_SIZE);
-            const dotExtension = match[7];
-            const originalKey = match[1] + dotExtension;
-            const newKey = match[0]; //whatever they requested is what we'll make
+            if (match === null) {
+                throw new BadRequest(`Key: '${requestedImageKey}' is not a supported image file!`);
+            }
+            else {
+                const dimensions = match[4]; //400xauto, for example.
+                const width = match[5] === 'auto' ? null : Math.min(parseInt(match[5], 10), MAX_SIZE);
+                const height = match[6] === 'auto' ? null : Math.min(parseInt(match[6], 10), MAX_SIZE);
+                const dotExtension = match[7];
+                const originalKey = match[1] + dotExtension;
+                const newKey = match[0]; //whatever they requested is what we'll make
 
-            console.log("Dimensions " + dimensions);
-            console.log("Width " + width);
-            console.log("Height " + height);
-            console.log("DotExtension " + dotExtension);
-            console.log("OriginalKey " + originalKey);
-            console.log("NewKey " + newKey);
+                console.log("Dimensions " + dimensions);
+                console.log("Width " + width);
+                console.log("Height " + height);
+                console.log("DotExtension " + dotExtension);
+                console.log("OriginalKey " + originalKey);
+                console.log("NewKey " + newKey);
 
-            callback(undefined, {
-                dimensions: dimensions,
-                width: width,
-                height: height,
-                originalKey: originalKey,
-                newKey: newKey
-            });
-        }
+
+                return resolve({
+                    dimensions: dimensions,
+                    width: width,
+                    height: height,
+                    originalKey: originalKey,
+                    newKey: newKey
+                });
+            }
+        })
     }
 
-    manipulate(parsedParameters, callback) {
-        this.s3.getObject({Bucket: this.bucket, Key: parsedParameters.originalKey}).promise()
+    manipulate(parsedParameters) {
+        return this.s3.getObject({Bucket: this.bucket, Key: parsedParameters.originalKey}).promise()
             .then(data => this.imageTransformer.transformImage(data.Body,
                 parsedParameters.width,
                 parsedParameters.height)
@@ -67,42 +70,54 @@ module.exports = class ThumbnailGenerator {
                     CacheControl: `max-age=${MAX_AGE}`,
                 }).promise()
             )
-            .then(() => callback(undefined, new MovedPermanently('', {
+            .then(() => new MovedPermanently('', {
                     location: `${this.url}/${parsedParameters.newKey}`,
                     'Cache-Control': 'no-cache, no-store, must-revalidate',
                     Pragma: 'no-cache',
                     Expires: '0'
-                }))
-            )
-            .catch(err => callback(new InternalServerError(err)));
+                })
+            );
     }
 
     generate(event, callback) {
         console.log(util.inspect(event, {depth: 5}));
 
-        let tasks = [];
+        return ThumbnailGenerator.parseRequestedImage(event.queryStringParameters.key)
+            .then((parsedParameters) => {
+                if (this.allowedDimensions.size > 0 && !this.allowedDimensions.has(parsedParameters.dimensions)) {
+                    throw new BadRequest(`Invalid dimensions specified: ${parsedParameters.dimensions}. ` +
+                        `Valid dimensions are: ${this.allowedDimensions}`);
+                }
+                else {
+                    return this.manipulate(parsedParameters);
+                }
+            })
+            .then((response) => callback(undefined, response))
+            .catch(err => callback(new InternalServerError(err)));
 
-        tasks.push((callback) => {
-            ThumbnailGenerator.parseRequestedImage(event.queryStringParameters.key, callback);
-        });
-
-        tasks.push((parsedParameters, callback) => {
-            if (this.allowedDimensions.size > 0 && !this.allowedDimensions.has(parsedParameters.dimensions)) {
-                callback(new BadRequest(`Invalid dimensions specified: ${parsedParameters.dimensions}. ` +
-                    `Valid dimensions are: ${this.allowedDimensions}`));
-            }
-            else {
-                this.manipulate(parsedParameters, callback);
-            }
-        });
-
-        async.waterfall(tasks, (err, data) => {
-            if (err) {
-                err.build(callback)
-            }
-            else {
-                data.build(callback);
-            }
-        });
+        // let tasks = [];
+        //
+        // tasks.push((callback) => {
+        //     ThumbnailGenerator.parseRequestedImage(event.queryStringParameters.key, callback);
+        // });
+        //
+        // tasks.push((parsedParameters, callback) => {
+        //     if (this.allowedDimensions.size > 0 && !this.allowedDimensions.has(parsedParameters.dimensions)) {
+        //         callback(new BadRequest(`Invalid dimensions specified: ${parsedParameters.dimensions}. ` +
+        //             `Valid dimensions are: ${this.allowedDimensions}`));
+        //     }
+        //     else {
+        //         this.manipulate(parsedParameters, callback);
+        //     }
+        // });
+        //
+        // async.waterfall(tasks, (err, data) => {
+        //     if (err) {
+        //         err.build(callback)
+        //     }
+        //     else {
+        //         data.build(callback);
+        //     }
+        // });
     }
 };
--- lambda/test/thumbnail-generator.js
+++ lambda/test/thumbnail-generator.js
@@ -1,21 +1,27 @@
 'use strict';
 
-let expect = require('chai').expect;
-let Base64Handler = require('../lib/thumbnail-generator');
+const expect = require('chai').expect;
+const assert = require('chai').assert;
+
+const ThumbnailGenerator = require('../lib/thumbnail-generator');
+
+const MockEvent = {
+    queryStringParameters: {
+        key: 'key'
+    }
+};
 
 describe("Thumbnail Generator", function () {
     it("Gets the correct mime type and image", function () {
-        let fakeImage = "data:test/1234;base64,testing";
-        let base64Handler = new Base64Handler(fakeImage);
-        expect(base64Handler.mimeType).to.deep.equal({type: "test", subtype: "1234"});
-        expect(base64Handler.base64Image).to.equal("testing");
-    });
-
-    it("Throws a regex mismatch exception", function() {
-        let fakeImage = "blah";
-        let throwTest = function() {
-            new Base64Handler(fakeImage);
-        };
-        expect(throwTest).to.throw(Base64RegexMismatch);
+        let thumbnailGenerator = new ThumbnailGenerator();
+        return thumbnailGenerator.generate(MockEvent, (err, data) => {
+            if (err) {
+                assert(!err,err)
+            }
+        }).then((data) => {
+            expect(data).to.equal('ass')
+        }).catch((err)=>{
+            assert(!err)
+        })
     });
 });
