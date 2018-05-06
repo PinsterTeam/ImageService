db14896dbce408b427bc296ef407be0923ae131b
scytherswings <scytherswings@gmail.com>
2018-05-06 12:42:36 -0400

    almost promises

--- lambda/lib/image-transformer.js
+++ lambda/lib/image-transformer.js
@@ -2,11 +2,11 @@ const Sharp = require('sharp');
 
 // Make this not static, should be like a no args constructor or something.
 module.exports = class ImageTransformer {
-   static transformImage(body, width, height, format = 'jpeg') {
+    static transformImage(body, width, height, format = 'jpeg') {
         return Sharp(body)
             .resize(width, height)
             .max()
             .toFormat(format)
-            .toBuffer()
+            .toBuffer();
     }
 };
--- lambda/test/thumbnail-generator.js
+++ lambda/test/thumbnail-generator.js
@@ -2,26 +2,30 @@
 
 const expect = require('chai').expect;
 const assert = require('chai').assert;
+const util = require('util');
 
 const ThumbnailGenerator = require('../lib/thumbnail-generator');
 
 const MockEvent = {
     queryStringParameters: {
-        key: 'key'
+        key: 'bob/key_400x200.jpg'
+    }
+};
+const MockS3 = class MockS3 {
+    getObject(s3Object) {
+        return new Promise(() =>)
     }
 };
 
-describe("Thumbnail Generator", function () {
-    it("Gets the correct mime type and image", function () {
+describe('Thumbnail Generator', function () {
+    it('Gets the correct mime type and image', function () {
         let thumbnailGenerator = new ThumbnailGenerator();
-        return thumbnailGenerator.generate(MockEvent, (err, data) => {
-            if (err) {
-                assert(!err,err)
-            }
-        }).then((data) => {
-            expect(data).to.equal('ass')
-        }).catch((err)=>{
-            assert(!err)
-        })
+        return thumbnailGenerator.generate(MockEvent, (data, err) => {
+            expect(data).to.equal('ass');
+        }).catch((err) => {
+            console.log(util.inspect(err, {depth: 5}));
+
+            expect(err).to.equal(undefined);
+        });
     });
 });
