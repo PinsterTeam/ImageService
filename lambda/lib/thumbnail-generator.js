f8f3fd1687b97ebbe3403add79d18c8d9c2f9498
scytherswings <scytherswings@gmail.com>
2018-05-06 13:25:04 -0400

    no promises, just callbacks. test pass

--- lambda/lib/thumbnail-generator.js
+++ lambda/lib/thumbnail-generator.js
@@ -11,113 +11,125 @@ const ImageTransformer = require('./image-transformer');
 
 const MAX_AGE = 86400; // 24 hours
 const MAX_SIZE = 5000; // 5 thousand pixels (wide or high)
-const IMAGE_KEY_PATTERN_REGEX = /(([\/a-zA-Z0-9]+)\/([a-zA-Z0-9]+))_((\d+|auto)x(\d+|auto))(\.jpeg|\.jpg|\.png)/;
+const IMAGE_KEY_PATTERN_REGEX = /(([/a-zA-Z0-9]+)\/([a-zA-Z0-9]+))_((\d+|auto)x(\d+|auto))(\.jpeg|\.jpg|\.png)/;
 
 module.exports = class ThumbnailGenerator {
-    constructor(s3, bucket, url, allowedDimensions, imageTransformer) {
-        this.s3 = _.isUndefined(s3) ? new AWSS3() : s3;
+    constructor(bucket, url, s3, imageTransformer, allowedDimensions) {
         this.bucket = _.isUndefined(bucket) ? process.env.BUCKET : bucket;
         this.url = _.isUndefined(url) ? process.env.URL : url;
-        this.allowedDimensions = _.isUndefined(allowedDimensions) ? new Set() : allowedDimensions;
+        this.s3 = _.isUndefined(s3) ? new AWSS3() : s3;
         this.imageTransformer = _.isUndefined(imageTransformer) ? ImageTransformer : imageTransformer;
+        this.allowedDimensions = _.isUndefined(allowedDimensions) ? new Set() : allowedDimensions;
     }
 
-    static parseRequestedImage(requestedImageKey) {
-        return new Promise((resolve) => {
-            const match = requestedImageKey.match(IMAGE_KEY_PATTERN_REGEX);
+    static parseRequestedImage(requestedImageKey, callback) {
+        const match = requestedImageKey.match(IMAGE_KEY_PATTERN_REGEX);
 
-            if (match === null) {
-                throw new BadRequest(`Key: '${requestedImageKey}' is not a supported image file!`);
+        if (match === null) {
+            callback(new BadRequest(`Key: '${requestedImageKey}' is not a supported image file!`));
+        }
+        else {
+            const dimensions = match[4]; //400xauto, for example.
+            const width = match[5] === 'auto' ? null : Math.min(parseInt(match[5], 10), MAX_SIZE);
+            const height = match[6] === 'auto' ? null : Math.min(parseInt(match[6], 10), MAX_SIZE);
+            const dotExtension = match[7];
+            const originalKey = match[1] + dotExtension;
+            const newKey = match[0]; //whatever they requested is what we'll make
+
+            console.log('Dimensions ' + dimensions);
+            console.log('Width ' + width);
+            console.log('Height ' + height);
+            console.log('DotExtension ' + dotExtension);
+            console.log('OriginalKey ' + originalKey);
+            console.log('NewKey ' + newKey);
+
+            callback(undefined, {
+                dimensions: dimensions,
+                width: width,
+                height: height,
+                originalKey: originalKey,
+                newKey: newKey
+            });
+        }
+    }
+
+    upload(parsedParameters, callback) {
+        this.s3.putObject(
+            {
+                Body: parsedParameters.buffer,
+                Bucket: this.bucket,
+                ContentType: 'image/jpeg',
+                Key: parsedParameters.newKey,
+                CacheControl: `max-age=${MAX_AGE}`,
+            }, (err) => {
+                if (err) {
+                    callback(new InternalServerError(err));
+                }
+                else {
+                    callback(undefined, new MovedPermanently('',
+                        {
+                            location: `${this.url}/${parsedParameters.newKey}`,
+                            'Cache-Control': 'no-cache, no-store, must-revalidate',
+                            Pragma: 'no-cache',
+                            Expires: '0'
+                        }));
+                }
+            });
+    }
+
+    download(parsedParameters, callback) {
+        this.s3.getObject({Bucket: this.bucket, Key: parsedParameters.originalKey}, (err, data) => {
+            if (err) {
+                callback(new InternalServerError(err));
             }
             else {
-                const dimensions = match[4]; //400xauto, for example.
-                const width = match[5] === 'auto' ? null : Math.min(parseInt(match[5], 10), MAX_SIZE);
-                const height = match[6] === 'auto' ? null : Math.min(parseInt(match[6], 10), MAX_SIZE);
-                const dotExtension = match[7];
-                const originalKey = match[1] + dotExtension;
-                const newKey = match[0]; //whatever they requested is what we'll make
-
-                console.log("Dimensions " + dimensions);
-                console.log("Width " + width);
-                console.log("Height " + height);
-                console.log("DotExtension " + dotExtension);
-                console.log("OriginalKey " + originalKey);
-                console.log("NewKey " + newKey);
-
-
-                return resolve({
-                    dimensions: dimensions,
-                    width: width,
-                    height: height,
-                    originalKey: originalKey,
-                    newKey: newKey
-                });
+                parsedParameters.body = data.Body;
+                callback(undefined, parsedParameters);
             }
-        })
+        });
     }
 
-    manipulate(parsedParameters) {
-        return this.s3.getObject({Bucket: this.bucket, Key: parsedParameters.originalKey}).promise()
-            .then(data => this.imageTransformer.transformImage(data.Body,
-                parsedParameters.width,
-                parsedParameters.height)
-            )
-            .then(buffer => this.s3.putObject({
-                    Body: buffer,
-                    Bucket: this.bucket,
-                    ContentType: 'image/jpeg',
-                    Key: parsedParameters.newKey,
-                    CacheControl: `max-age=${MAX_AGE}`,
-                }).promise()
-            )
-            .then(() => new MovedPermanently('', {
-                    location: `${this.url}/${parsedParameters.newKey}`,
-                    'Cache-Control': 'no-cache, no-store, must-revalidate',
-                    Pragma: 'no-cache',
-                    Expires: '0'
-                })
-            );
-    }
 
     generate(event, callback) {
         console.log(util.inspect(event, {depth: 5}));
 
-        return ThumbnailGenerator.parseRequestedImage(event.queryStringParameters.key)
-            .then((parsedParameters) => {
-                if (this.allowedDimensions.size > 0 && !this.allowedDimensions.has(parsedParameters.dimensions)) {
-                    throw new BadRequest(`Invalid dimensions specified: ${parsedParameters.dimensions}. ` +
-                        `Valid dimensions are: ${this.allowedDimensions}`);
-                }
-                else {
-                    return this.manipulate(parsedParameters);
-                }
-            })
-            .then((response) => callback(undefined, response))
-            .catch(err => callback(new InternalServerError(err)));
+        let self = this;
+        let tasks = [];
 
-        // let tasks = [];
-        //
-        // tasks.push((callback) => {
-        //     ThumbnailGenerator.parseRequestedImage(event.queryStringParameters.key, callback);
-        // });
-        //
-        // tasks.push((parsedParameters, callback) => {
-        //     if (this.allowedDimensions.size > 0 && !this.allowedDimensions.has(parsedParameters.dimensions)) {
-        //         callback(new BadRequest(`Invalid dimensions specified: ${parsedParameters.dimensions}. ` +
-        //             `Valid dimensions are: ${this.allowedDimensions}`));
-        //     }
-        //     else {
-        //         this.manipulate(parsedParameters, callback);
-        //     }
-        // });
-        //
-        // async.waterfall(tasks, (err, data) => {
-        //     if (err) {
-        //         err.build(callback)
-        //     }
-        //     else {
-        //         data.build(callback);
-        //     }
-        // });
+        tasks.push((callback) => {
+            ThumbnailGenerator.parseRequestedImage(event.queryStringParameters.key, callback);
+        });
+
+        tasks.push((parsedParameters, callback) => {
+            if (self.allowedDimensions.size > 0 && !self.allowedDimensions.has(parsedParameters.dimensions)) {
+                callback(new BadRequest(`Invalid dimensions specified: ${parsedParameters.dimensions}. ` +
+                    `Valid dimensions are: ${self.allowedDimensions}`));
+            }
+            else {
+                callback(undefined, parsedParameters);
+            }
+        });
+
+        tasks.push((parsedParameters, callback) => {
+            this.download(parsedParameters, callback);
+        });
+
+        tasks.push((parsedParameters, callback) => {
+            this.imageTransformer.transformImage(parsedParameters, callback);
+        });
+
+
+        tasks.push((parsedParameters, callback) => {
+            this.upload(parsedParameters, callback);
+        });
+
+        async.waterfall(tasks, (err, data) => {
+            if (err) {
+                err.build(callback);
+            }
+            else {
+                data.build(callback);
+            }
+        });
     }
 };
--- lambda/test/thumbnail-generator.js
+++ lambda/test/thumbnail-generator.js
@@ -12,20 +12,47 @@ const MockEvent = {
     }
 };
 const MockS3 = class MockS3 {
-    getObject(s3Object) {
-        return new Promise(() =>)
+    getObject(s3Object, callback) {
+        callback(undefined, {Body: new Buffer([1, 2, 3, 4])});
+    }
+
+    putObject(s3Object, callback) {
+        callback();
     }
 };
 
+
+const MockImageTransformer = class MockImageTransformer {
+    transformImage(parsedParameters, callback) {
+        parsedParameters.buffer = new Buffer([1, 2, 3, 4]);
+
+        callback(undefined, parsedParameters);
+    }
+};
+
+const ExpectedResponse = {
+    statusCode: 301,
+    headers:
+        {
+            location: 'url/bob/key_400x200.jpg',
+            'Cache-Control': 'no-cache, no-store, must-revalidate',
+            Pragma: 'no-cache',
+            Expires: '0'
+        },
+    body: ''
+}
+
+
 describe('Thumbnail Generator', function () {
     it('Gets the correct mime type and image', function () {
-        let thumbnailGenerator = new ThumbnailGenerator();
-        return thumbnailGenerator.generate(MockEvent, (data, err) => {
-            expect(data).to.equal('ass');
-        }).catch((err) => {
+        let thumbnailGenerator = new ThumbnailGenerator('bucket', 'url', new MockS3(), new MockImageTransformer());
+        thumbnailGenerator.generate(MockEvent, (err, data) => {
             console.log(util.inspect(err, {depth: 5}));
 
             expect(err).to.equal(undefined);
+            console.log(util.inspect(data, {depth: 5}));
+
+            expect(data).to.deep.equal(ExpectedResponse);
         });
     });
 });
