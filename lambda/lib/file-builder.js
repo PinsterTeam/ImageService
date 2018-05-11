8bb31e1fb7fdf04323b2eaf2e3783b1f5009ce80
scytherswings <scytherswings@gmail.com>
2018-05-11 16:29:35 -0400

    refactored to allow contentType to be checked sort of

--- lambda/lib/file-builder.js
+++ lambda/lib/file-builder.js
@@ -3,21 +3,31 @@
 const Base64Handler = require('./base64-handler');
 const _ = require('lodash');
 const md5 = require('md5');
+const BadRequest = require('./bad-request');
 
 module.exports = class FileBuilder {
     constructor(base64Handler) {
         this.base64Handler = _.isUndefined(base64Handler) ? Base64Handler : base64Handler;
     }
 
-    getFile(image) {
-        let base64Handler = new this.base64Handler(image);
+    getFile(parsedRequest, callback) {
+        let base64Handler = new this.base64Handler(parsedRequest.image);
         let buffer = base64Handler.buffer;
         let mimeType = base64Handler.mimeType;
         let fileName = 'raw/' + md5(buffer);
-        return {
-            Key: fileName,
-            Body: buffer,
-            ContentType: `${mimeType.type}/${mimeType.subtype}`
-        };
+        let contentType = `${mimeType.type}/${mimeType.subtype}`;
+
+        if (mimeType.type === 'image') {
+            callback(undefined, {
+                Key: fileName,
+                Body: buffer,
+                ContentType: contentType,
+                Metadata: parsedRequest.metadata,
+                Bucket: parsedRequest.bucket
+            });
+        }
+        else {
+            callback(new BadRequest(`Files of type: ${contentType} are not supported.`));
+        }
     }
 };
