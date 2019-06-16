4f06f214c2c6988c62a6a57475e9e29d563e6ad3
scytherswings <scytherswings@gmail.com>
2019-06-16 13:25:49 -0400

    working to remove callback hell

--- lambda/lib/image-transformer.js
+++ lambda/lib/image-transformer.js
@@ -6,30 +6,28 @@ const InternalServerError = require('./internal-server-error');
 
 // Make this not static, should be like a no args constructor or something.
 module.exports = class ImageTransformer {
-    static transformImage(parsedParameters, callback) {
+    async transformImage(parsedParameters) {
         let body = parsedParameters.body;
         let width = parsedParameters.width;
         let height = parsedParameters.height;
         let format = _.isUndefined(parsedParameters.format) ? 'jpeg' : parsedParameters.format;
 
         if (body === null || width === null || height === null || format === null) {
-            callback(new InternalServerError('One of the required image transform parameters was null!'));
+            throw new InternalServerError('One of the required image transform parameters was null!');
         }
-        else {
-            Sharp(body)
-                .rotate()
-                .resize(width, height, {fit: 'inside'})
-                // .composite([{input: '../assets/pinster_watermark.svg', gravity: 'southeast'}])
-                .toFormat(format)
-                .toBuffer()
-                .then(data => {
-                    parsedParameters.buffer = data;
-                    callback(undefined, parsedParameters);
-                })
-                .catch(err => {
-                    console.error('Error transforming image', err);
-                    return new InternalServerError(err);
-                });
-        }
+        return Sharp(body)
+            .rotate()
+            .resize(width, height, {fit: 'inside'})
+            .composite([{input: '../assets/pinster_watermark.png', gravity: 'southeast'}])
+            .toFormat(format)
+            .toBuffer()
+            .then(data => {
+                parsedParameters.buffer = data;
+                return parsedParameters;
+            })
+            .catch(err => {
+                console.error('Error transforming image', err);
+                throw new InternalServerError(err);
+            });
     }
 };
---
+++ lambda/test/image-transformer.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const expect = require('chai').expect;
+const util = require('util');
+const path = require('path');
+
+const ImageTransformer = require('../lib/image-transformer');
+
+describe('Image Transformer', function () {
+    it('Can add a watermark to an image', function() {
+        let imageTransformer = new ImageTransformer
+    })
+});
